bd17917984f8faebfb13cefd2c957852fd72bd7c
PipainS <70841962+PipainS@users.noreply.github.com>
2023-05-31 17:46:06 +0300

    Add files via upload

---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создание стека с размером 5
+            Stack<int> stack = new Stack<int>(5);
+
+            Console.WriteLine($"Is stack empty? {stack.IsEmpty()}"); // Выводит True
+
+            stack.Push(10);
+            stack.Push(20);
+            stack.Push(30);
+            stack.Push(40);
+            stack.Push(50);
+            Console.WriteLine($"Is stack full? {stack.IsFull()}"); // Выводит True
+
+            Console.WriteLine($"Top element: {stack.Peek()}"); // Выводит 50
+
+            while (!stack.IsEmpty())
+            {
+                Console.WriteLine($"Popped element: {stack.Pop()}"); // Выводит 50, 40, 30, 20, 10
+            }
+
+            Console.WriteLine($"Is stack empty? {stack.IsEmpty()}"); // Выводит True
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Stack/Stack/Stack.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class Stack<T>
+    {
+        private T[] _array;
+        private int _top;
+
+        public Stack(int size)
+        {
+            _array = new T[size];
+            _top = 0;
+        }
+
+        public int Top => _top;
+
+        public int Size => _array.Length;
+
+        public bool IsFull()
+        {
+            return _top == _array.Length;
+        }
+
+        public bool IsEmpty()
+        {
+            return _top == 0;
+        }
+
+        public void Push(T element)
+        {
+            if (IsFull())
+                Resize(_array.Length * 2);
+            _array[_top++] = element;
+        }
+
+        public T? Peek()
+        {
+            if (!IsEmpty())
+                return _array[_top - 1];
+            return default(T);
+        }
+
+        public T? Pop()
+        {
+            if (!IsEmpty())
+                return _array[--_top];
+            return default(T);
+        }
+
+        private void Resize(int newSize)
+        {
+            T[] newArray = new T[newSize];
+            Array.Copy(_array, newArray, Math.Min(newSize, _array.Length));
+            _array = newArray;
+        }
+    }
+}
