e387f972a3fabbb77bb1cb493d5528c198fbabb1
PipainS <70841962+PipainS@users.noreply.github.com>
2023-06-26 13:11:49 +0300

    Add files via upload

---
+++ HashTable/HashTable/HashTable.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    class HashItem<K, T>
+    {
+        public K Key { get; set; } // Key
+        public T Value { get; set; } // Value
+
+        public HashItem(K key, T value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"(key={Key}, Value={Value})";
+        }
+    }
+
+    class HashTableOpen<K, T> where K : IComparable where T : IComparable
+    {
+        private int capacity; // Capacity of the hash table
+        private int size; // Number of elements in the hash table
+        private Dictionary<int, List<HashItem<K, T>>> items;
+
+        public IReadOnlyCollection<KeyValuePair<int, List<HashItem<K, T>>>>? Item => items?.ToList()?.AsReadOnly();
+
+        public HashTableOpen(int capacity)
+        {
+            this.capacity = capacity;
+            size = 0;
+            items = new Dictionary<int, List<HashItem<K, T>>>(this.capacity);
+        }
+
+        private int GetHash(K key)
+        {
+            return Math.Abs(key.GetHashCode()) % capacity;
+        }
+
+        private void Resize(int newCapacity)
+        {
+            var newItems = new Dictionary<int, List<HashItem<K, T>>>(newCapacity);
+
+            foreach (var entry in items)
+            {
+                foreach (var item in entry.Value)
+                {
+                    int newIndex = Math.Abs(item.Key.GetHashCode() % newCapacity);
+
+                    if (!newItems.ContainsKey(newIndex))
+                    {
+                        newItems[newIndex] = new List<HashItem<K, T>>();
+                    }
+
+                    newItems[newIndex].Add(item);
+                }
+            }
+
+            items = newItems;
+            capacity = newCapacity;
+        }
+
+        public void Insert(K key, T value)
+        {
+            var item = new HashItem<K, T>(key, value);
+            var hash = GetHash(key);
+
+            if (items.ContainsKey(hash))
+            {
+                var hashTableItem = items[hash];
+
+                if (hashTableItem.Any(i => i.Key.CompareTo(item.Key) == 0))
+                {
+                    Console.WriteLine("Value already exists.");
+                    return;
+                }
+
+                hashTableItem.Add(item);
+            }
+            else
+            {
+                var hashTableItem = new List<HashItem<K, T>> { item };
+                items.Add(hash, hashTableItem);
+            }
+
+            size++;
+
+            if (size > capacity)
+            {
+                Resize(capacity * 2);
+            }
+        }
+
+        public void Delete(K key)
+        {
+            var hash = GetHash(key);
+
+            if (!items.ContainsKey(hash))
+            {
+                return;
+            }
+
+            var hashTableItem = items[hash];
+            var item = hashTableItem.SingleOrDefault(i => i.Key.CompareTo(key) == 0);
+
+            if (item != null)
+            {
+                hashTableItem.Remove(item);
+                size--;
+
+                if (size < capacity / 4)
+                {
+                    Resize(capacity / 2);
+                }
+            }
+        }
+
+        public HashItem<K, T>? Search(K key)
+        {
+            var hash = GetHash(key);
+
+            if (!items.ContainsKey(hash))
+            {
+                return null;
+            }
+
+            var hashTableItem = items[hash];
+
+            return hashTableItem.SingleOrDefault(i => i.Key.CompareTo(key) == 0);
+        }
+
+        public void View()
+        {
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.Key);
+
+                foreach (var value in item.Value)
+                {
+                    Console.WriteLine($"value: {value.Key}, key: {value.Value}");
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+    class HashTableClose<K, T>
+    {
+        private int capacity;
+        private int size;
+        private HashItem<K, T>[] items;
+
+        public int Size => size;
+        public int Capacity => capacity;
+
+        public bool IsEmpty() => size == 0;
+
+        public HashTableClose(int capacity)
+        {
+            this.capacity = capacity;
+            size = 0;
+            items = new HashItem<K, T>[capacity];
+        }
+
+        private int GetHash(K key)
+        {
+            return Math.Abs(key.GetHashCode()) % capacity;
+        }
+
+        public void Insert(K key, T value)
+        {
+            if (size == capacity)
+            {
+                Resize(capacity * 2);
+            }
+
+            int index = GetHash(key);
+
+            while (items[index] != null && !EqualityComparer<K>.Default.Equals(items[index].Key, key))
+            {
+                index = (index + 1) % capacity;
+            }
+
+            items[index] = new HashItem<K, T>(key, value);
+            size++;
+        }
+
+        public T Search(K key)
+        {
+            int index = GetHash(key);
+
+            while (items[index] != null && !EqualityComparer<K>.Default.Equals(items[index].Key, key))
+            {
+                index = (index + 1) % capacity;
+            }
+
+            if (items[index] == null)
+            {
+                throw new KeyNotFoundException();
+            }
+
+            return items[index].Value;
+        }
+
+        public void View()
+        {
+            foreach (var item in items)
+            {
+                if (item != null)
+                {
+                    Console.WriteLine($"Key: {item.Key}, Value: {item.Value}");
+                }
+            }
+        }
+
+        public bool Delete(K key)
+        {
+            int index = GetHash(key);
+
+            while (items[index] != null && !EqualityComparer<K>.Default.Equals(items[index].Key, key))
+            {
+                index = (index + 1) % capacity;
+            }
+
+            if (items[index] == null)
+            {
+                return false;
+            }
+            else
+            {
+                items[index] = null;
+                size--;
+                return true;
+            }
+        }
+
+        private void Resize(int newCapacity)
+        {
+            HashItem<K, T>[] newItems = new HashItem<K, T>[newCapacity];
+
+            for (int i = 0; i < capacity; i++)
+            {
+                if (items[i] != null)
+                {
+                    K key = items[i].Key;
+                    T value = items[i].Value;
+                    int newIndex = Math.Abs(key.GetHashCode()) % newCapacity;
+
+                    while (newItems[newIndex] != null && !EqualityComparer<K>.Default.Equals(newItems[newIndex].Key, key))
+                    {
+                        newIndex = (newIndex + 1) % newCapacity;
+                    }
+
+                    newItems[newIndex] = new HashItem<K, T>(key, value);
+                }
+            }
+
+            capacity = newCapacity;
+            items = newItems;
+        }
+    }
+}
---
+++ HashTable/HashTable/Program.cs
@@ -0,0 +1,73 @@
+﻿using HashTable;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Data_Structures_and_Algorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Test HashTableOpen
+            Console.WriteLine("Testing HashTableOpen:");
+            HashTableOpen<string, int> hashTableOpen = new HashTableOpen<string, int>(5);
+
+            // Insert elements
+            hashTableOpen.Insert("apple", 1);
+            hashTableOpen.Insert("banana", 2);
+            hashTableOpen.Insert("orange", 3);
+            hashTableOpen.Insert("grape", 4);
+            hashTableOpen.Insert("kiwi", 5);
+
+            // Search elements
+            Console.WriteLine("Search 'apple': " + hashTableOpen.Search("apple"));
+            Console.WriteLine("Search 'banana': " + hashTableOpen.Search("banana"));
+            Console.WriteLine("Search 'orange': " + hashTableOpen.Search("orange"));
+            Console.WriteLine("Search 'grape': " + hashTableOpen.Search("grape"));
+            Console.WriteLine("Search 'kiwi': " + hashTableOpen.Search("kiwi"));
+
+            // Delete elements
+            Console.WriteLine("Delete 'banana': ");
+            hashTableOpen.Delete("banana");
+            Console.WriteLine("Delete 'kiwi': ");
+            hashTableOpen.Delete("kiwi");
+ 
+            // View the updated hash table
+            Console.WriteLine("Updated HashTableOpen:");
+            hashTableOpen.View();
+            Console.WriteLine();
+
+
+            // Test HashTableClose
+            Console.WriteLine("Testing HashTableClose:");
+            HashTableClose<string, int> hashTableClose = new HashTableClose<string, int>(5);
+
+            // Insert elements
+            hashTableClose.Insert("apple", 1);
+            hashTableClose.Insert("banana", 2);
+            hashTableClose.Insert("orange", 3);
+            hashTableClose.Insert("grape", 4);
+            hashTableClose.Insert("kiwi", 5);
+
+            // Search elements
+            Console.WriteLine("Search 'apple': " + hashTableClose.Search("apple"));
+            Console.WriteLine("Search 'banana': " + hashTableClose.Search("banana"));
+            Console.WriteLine("Search 'orange': " + hashTableClose.Search("orange"));
+            Console.WriteLine("Search 'grape': " + hashTableClose.Search("grape"));
+            Console.WriteLine("Search 'kiwi': " + hashTableClose.Search("kiwi"));
+
+            // Delete elements
+            Console.WriteLine("Delete 'banana': " + hashTableClose.Delete("banana"));
+            Console.WriteLine("Delete 'kiwi': " + hashTableClose.Delete("kiwi"));
+
+            // View the updated hash table
+            Console.WriteLine("Updated HashTableClose:");
+            hashTableClose.View();
+
+        }
+    }
+}
